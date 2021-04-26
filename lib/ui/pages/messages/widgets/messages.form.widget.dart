6df84fa47dac6ad7cf0c5dca3c267f634b8377a4
Aymane REIDA <reida.aymane@gmail.com>
2021-04-26 08:14:50 +0000

    Full commit

---
+++ lib/ui/pages/messages/widgets/messages.form.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MessagesFormWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      alignment: Alignment.bottomLeft,
+      child: Text('Messages Form'),
+    );
+  }
+}
