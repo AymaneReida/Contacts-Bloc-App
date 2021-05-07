4639a41f5149ab38eb884418e3fcfa95a3d9189c
Aymane REIDA <reida.aymane@gmail.com>
2021-05-07 21:30:01 +0000

    Changes commit

--- lib/ui/pages/contactsWithMessages/contacts.messages.page.dart
+++ lib/ui/pages/contactsWithMessages/contacts.messages.page.dart
@@ -5,16 +5,16 @@ import 'package:contacts_bloc_app/bloc/messages/messages.actions.dart';
 import 'package:contacts_bloc_app/bloc/messages/messages.bloc.dart';
 import 'package:contacts_bloc_app/bloc/messages/messages.state.dart';
 import 'package:contacts_bloc_app/enums/enums.dart';
+import 'package:contacts_bloc_app/ui/pages/contactsWithMessages/widgets/contacts.list.widget.dart';
 import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.form.widget.dart';
 import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.list.widget.dart';
+import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.widget.dart';
 import 'package:contacts_bloc_app/ui/shared/circular.progress.indicator.widget.dart';
 import 'package:contacts_bloc_app/ui/shared/error.retry.action.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ContactsWithMessages extends StatelessWidget {
-  ScrollController scrollController = new ScrollController();
-
   @override
   Widget build(BuildContext context) {
     context.read<ContactsBloc>().add(new LoadAllContactsEvent());
@@ -24,91 +24,8 @@ class ContactsWithMessages extends StatelessWidget {
       ),
       body: Column(
         children: [
-          BlocBuilder<ContactsBloc, ContactsState>(builder: (context, state) {
-            if (state.requestState == RequestState.LOADING) {
-              return MyCircularProgressIndicatorIndicatorWidget();
-            } else if (state.requestState == RequestState.ERROR) {
-              return ErrorRetryMessage(
-                errorMessage: state.errorMessage,
-                onAction: () {
-                  context.read<ContactsBloc>().add(state.currentEvent);
-                },
-              );
-            } else if (state.requestState == RequestState.LOADED) {
-              return SizedBox(
-                height: 150,
-                child: ListView.builder(
-                  controller: scrollController,
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
-                      onTap: () {
-                        context.read<MessagesBloc>().add(
-                            new MessagesByContactsEvent(state.contacts[index]));
-                        scrollController.animateTo(
-                            (index * 200 - 150).toDouble(),
-                            duration: Duration(microseconds: 2000),
-                            curve: Curves.ease);
-                      },
-                      child: BlocBuilder<MessagesBloc, MessagesState>(
-                        builder: (context, messagesState) => Card(
-                          elevation: 1,
-                          child: Container(
-                            padding: EdgeInsets.all(16),
-                            width: 150,
-                            child: Column(children: [
-                              CircleAvatar(
-                                child: Text('${state.contacts[index].profile}'),
-                              ),
-                              Text('${state.contacts[index].name}'),
-                              Text('${state.contacts[index].score}'),
-                            ]),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: (messagesState.currentContact ==
-                                        state.contacts[index])
-                                    ? 3
-                                    : 1,
-                                color: Color(0xff5e2750),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  itemCount: state.contacts.length,
-                  scrollDirection: Axis.horizontal,
-                ),
-              );
-            } else {
-              return Container();
-            }
-          }),
-          Expanded(
-            child: BlocBuilder<MessagesBloc, MessagesState>(
-              builder: (context, state) {
-                if (state.requestState == RequestState.LOADING) {
-                  return MyCircularProgressIndicatorIndicatorWidget();
-                } else if (state.requestState == RequestState.ERROR) {
-                  return ErrorRetryMessage(
-                    errorMessage: state.errorMessage,
-                    onAction: () {
-                      context
-                          .read<MessagesBloc>()
-                          .add(state.currentMessageEvent);
-                    },
-                  );
-                } else if (state.requestState == RequestState.LOADED) {
-                  return MessagesListWidget(
-                    messages: state.messages,
-                  );
-                } else if (state.requestState == RequestState.NONE) {
-                  return Container();
-                }
-              },
-            ),
-          ),
+          ContactsListHorizontalWidget(),
+          MessagesWidget(),
           BlocBuilder<ContactsBloc, ContactsState>(
             builder: (context, state) => MessagesFormWidget(
               contact: state.currentContact,
--- lib/ui/pages/messages/messages.page.dart
+++ lib/ui/pages/messages/messages.page.dart
@@ -6,6 +6,7 @@ import 'package:contacts_bloc_app/model/contact.model.dart';
 import 'package:contacts_bloc_app/ui/pages/messages/widgets/contact.info.widget.dart';
 import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.form.widget.dart';
 import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.list.widget.dart';
+import 'package:contacts_bloc_app/ui/pages/messages/widgets/messages.widget.dart';
 import 'package:contacts_bloc_app/ui/shared/circular.progress.indicator.widget.dart';
 import 'package:contacts_bloc_app/ui/shared/error.retry.action.dart';
 import 'package:flutter/material.dart';
@@ -51,30 +52,7 @@ class MessagesPage extends StatelessWidget {
           ContactInfoWidget(
             contact: this.contact,
           ),
-          Expanded(
-            child: BlocBuilder<MessagesBloc, MessagesState>(
-              builder: (context, state) {
-                if (state.requestState == RequestState.LOADING) {
-                  return MyCircularProgressIndicatorIndicatorWidget();
-                } else if (state.requestState == RequestState.ERROR) {
-                  return ErrorRetryMessage(
-                    errorMessage: state.errorMessage,
-                    onAction: () {
-                      context
-                          .read<MessagesBloc>()
-                          .add(state.currentMessageEvent);
-                    },
-                  );
-                } else if (state.requestState == RequestState.LOADED) {
-                  return MessagesListWidget(
-                    messages: state.messages,
-                  );
-                } else if (state.requestState == RequestState.NONE) {
-                  return Container();
-                }
-              },
-            ),
-          ),
+          MessagesWidget(),
           MessagesFormWidget(
             contact: this.contact,
           ),
